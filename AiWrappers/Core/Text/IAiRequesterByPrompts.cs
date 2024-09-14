549790153b158795b3ad7b51495904158dda4068
scherenhaenden <scherenhaenden@gmail.com>
2024-09-14 17:52:24 +0200

    Update project dependencies and version to 0.0.6; add RunRequestWithResult method to IAiRequesterByPrompts interface

--- AiWrappers/Core/Text/IAiRequesterByPrompts.cs
+++ AiWrappers/Core/Text/IAiRequesterByPrompts.cs
@@ -3,4 +3,6 @@ namespace AiWrappers.Core.Text;
 public interface IAiRequesterByPrompts
 {
     public Task<string?> RunRequest(string prompt);
+    
+    public Task<string?> RunRequestWithResult(string prompt);
 }
